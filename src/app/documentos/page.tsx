746a3972c23762ed1e9724543a2de3335d288cb9
RichartR <rubiorichart@gmail.com>
2026-08-08 18:37:47 +0200

    fix

--- src/app/documentos/page.tsx
+++ src/app/documentos/page.tsx
@@ -13,7 +13,7 @@ export default function DocumentosPage() {
           ]}
         />
         <div className="flex items-center gap-4">
-          <div className="w-12 h-12 bg-oro/[0.03] border border-oro/10 flex items-center justify-center">
+          <div className="w-12 h-12 flex items-center justify-center">
             <img src="/assets/icons/shuriken.png" className="w-6 sm:w-7 xl:w-8 h-auto object-contain" alt="icon" />
           </div>
           <h1 className="text-xl xl:text-2xl font-black text-oro uppercase tracking-[0.3em]">
