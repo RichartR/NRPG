e24f85e03b692e5f05471a61db9b6677d9432d4d
RichartR <136321251+RichartR@users.noreply.github.com>
2026-09-25 00:17:36 +0200

    Extend cache fallback for src/app/mundo-ninja

--- src/app/mundo-ninja/page.tsx
+++ src/app/mundo-ninja/page.tsx
@@ -1,4 +1,4 @@
-export const revalidate = 300;
+export const revalidate = 3600;
 
 import { MasterServerService } from '@/services/supabase/master.server.service';
 import MundoNinjaClientView from './MundoNinjaClientView';
